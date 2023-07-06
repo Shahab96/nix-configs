b1933517b854417c9d9b9f4d59dc294f993ba57f
Shahab Dogar <shahab@dogar.dev>
2023-07-05 23:15:29 -0700

    formatting

--- configuration.nix
+++ configuration.nix
@@ -116,8 +116,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     firefox
     discord
     alacritty
@@ -167,5 +165,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-
 }
